f9f01a99497c88bc8d506b8c80d60510161ffc79
DESKTOP-1ALVEB7\Rynoz <nhannt.itech@gmail.com>
2021-10-12 18:36:43 +0700

    fix [back-end] clean code

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,21 +1,13 @@
 const { auth } = require('./middleware/auth');
 const express = require('express');
-const multer = require('multer');
 
 const UserController = require('./controllers/UserController');
-const EventController = require('./controllers/EventController');
-const uploadConfig = require('./config/upload');
 
 const routes = express.Router();
-const upload = multer(uploadConfig);
 
 routes.get('/status', (req, res) => {
 	res.send({ status: 300 });
 });
-
-//Event
-routes.get('/event/:eventId', EventController.getEventById);
-routes.post('/event', upload.single('thumbnail'), EventController.createEvent);
 
 //User
 routes.post('/user/register', UserController.register);
