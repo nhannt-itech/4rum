8bcee0a57991110c5d65c612ed5dc838d0604f47
DESKTOP-1ALVEB7\Rynoz <nhannt.itech@gmail.com>
2021-10-22 12:30:36 +0700

    fix: reload

--- server.js
+++ server.js
@@ -42,10 +42,12 @@ try {
 }
 
 //build mode
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "/client/public/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
+	app.get("*", function (req, res) {
+		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+	});
+}
 
 var server = app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`);
