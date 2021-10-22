2bfcc5e6745b670cb4805f0d98e79ccbc0106a36
DESKTOP-1ALVEB7\Rynoz <nhannt.itech@gmail.com>
2021-10-22 11:13:50 +0700

    fix: deploy

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const routes = require("./routes");
 const path = require("path");
-require("dotenv").config();
 const errorHandler = require("./middleware/error-handler");
 const app = express();
 const ChatController = require("./controllers/ChatController");
 
+if (process.env.NODE_ENV !== "production") require("dotenv").config;
 const PORT = process.env.PORT || 5000;
 
 var corsOptions = {
@@ -26,16 +26,10 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
 app.use(cookieParser());
 app.use(morgan("tiny"));
 app.use(routes);
 app.use(errorHandler);
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-// if (process.env.NODE_ENV !== "production") {
-// 	require("dotenv").config();
-// }
 
 try {
 	mongoose.connect(process.env.MONGODB_URI, {
@@ -47,9 +41,10 @@ try {
 	console.log(error);
 }
 
+//build mode
 app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", function (req, res) {
-	res.sendFile(path.join(__dirname, "client/build", "index.html"));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/client/public/index.html"));
 });
 
 var server = app.listen(PORT, () => {
