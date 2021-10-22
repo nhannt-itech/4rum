19faa59b3b51da1ff68a0a6cbe02272a1a3354d2
DESKTOP-1ALVEB7\Rynoz <nhannt.itech@gmail.com>
2021-10-22 11:38:55 +0700

    test: deploy

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import queryString from "querystring";
 
 const axiosClient = axios.create({
-	baseURL: `${process.env.REACT_APP_API_URL}`,
 	headers: {
 		"Content-Type": "application/json",
 	},
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var corsOptions = {
 	credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
